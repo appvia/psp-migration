0cfbd61807a2e7c5b764a78f6b9e67ff1dd6d72e
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-12-06 22:03:49 +0000

    code and ci

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,38 +1,76 @@
-// jest.mock('../src/github')
 import * as mod from '../index'
+import * as fs from 'fs'
 
-// let processExitSpy
-// let consoleSpy
-
+const fixturePSPYAML = `
+apiVersion: policy/v1beta1
+kind: PodSecurityPolicy
+metadata:
+  name: policy
+spec:
+  runAsUser:
+    rule: RunAsAny
+  seLinux:
+    rule: RunAsAny
+  fsGroup:
+    rule: RunAsAny
+  supplementalGroups:
+    rule: 'RunAsAny'
+  volumes:
+    - '*'
+`
+const fixturePSPJSON = `{ "apiVersion": "policy/v1beta1", "kind": "PodSecurityPolicy", "metadata": { "name": "policy" }, "spec": { "runAsUser": { "rule": "RunAsAny" }, "seLinux": { "rule": "RunAsAny" }, "fsGroup": { "rule": "RunAsAny" }, "supplementalGroups": { "rule": "RunAsAny" }, "volumes": ["*"] } }`
+const fixturePSPObject = { "apiVersion": "policy/v1beta1", "kind": "PodSecurityPolicy", "metadata": { "name": "policy" }, "spec": { "runAsUser": { "rule": "RunAsAny" }, "seLinux": { "rule": "RunAsAny" }, "fsGroup": { "rule": "RunAsAny" }, "supplementalGroups": { "rule": "RunAsAny" }, "volumes": ["*"] } }
 
 describe('parse', () => {
 
-  it('should parse a yaml object', () => {
-    const yaml = `
-    apiVersion: policy/v1beta1
-    kind: PodSecurityPolicy
-    metadata:
-      name: policy
-    spec:
-      runAsUser:
-        rule: RunAsAny
-      seLinux:
-        rule: RunAsAny
-      fsGroup:
-        rule: MustRunAs
-        ranges:
-          - min: 100
-            max: 200
-      supplementalGroups:
-        rule: 'RunAsAny'
-      volumes:
-        - '*'
-    `
-    return expect(mod.parse(yaml)).toMatchSnapshot()
-  })
+  it('should parse a yaml object', () => expect(mod.parse(fixturePSPYAML)).toMatchSnapshot())
 
-  it('should parse a json object', () => {
-    const json = `{ "apiVersion": "policy/v1beta1", "kind": "PodSecurityPolicy", "metadata": { "name": "policy" }, "spec": { "runAsUser": { "rule": "RunAsAny" }, "seLinux": { "rule": "RunAsAny" }, "fsGroup": { "rule": "MustRunAs", "ranges": [{ "min": 100, "max": 200 }] }, "supplementalGroups": { "rule": "RunAsAny" }, "volumes": ["*"] } }`
-    return expect(mod.parse(json)).toMatchSnapshot()
+  it('should parse a json object', () => expect(mod.parse(fixturePSPJSON)).toMatchSnapshot())
+})
+
+describe('transform', () => {
+  it('should call the right engine', () => {
+    const spy = jest.spyOn(mod, "transform_gatekeeper")
+    mod.transform(fixturePSPObject, 'gatekeeper')
+    expect(spy).toHaveBeenCalled()
   })
+})
+
+function help_load_psp(fixture: string) {
+  const yaml = fs.readFileSync(`tests/${fixture}/psp.yaml`, { flag: 'r' })
+  return mod.parse(yaml.toString())
+}
+
+const pspFields = [
+  "allowPrivilegeEscalation",
+  "allowedCapabilities",
+  "allowedFlexVolumes",
+  "allowedHostPaths",
+  "allowedProcMountTypes",
+  "allowedUnsafeSysctls",
+  "apparmor",
+  "defaultAddCapabilities",
+  "defaultAllowPrivilegeEscalation",
+  "forbiddenSysctls",
+  "fsgroup",
+  "hostIPC",
+  "hostNetwork",
+  "hostPID",
+  "hostPorts",
+  "privileged",
+  "readOnlyRootFilesystem",
+  "requiredDropCapabilities",
+  "runAsGroup",
+  "runAsUser",
+  "seLinux",
+  "seccomp",
+  "supplementalGroups",
+  "volumes"
+].map(field => [field])
+
+describe('transform_gatekeeper', () => {
+  it('should do an empty PSP', () => expect(mod.transform_gatekeeper(fixturePSPObject)).toStrictEqual([]))
+  test.each(pspFields)('%s', (field) =>
+    expect(mod.transform_gatekeeper(help_load_psp(field))).toMatchSnapshot()
+  )
 })
