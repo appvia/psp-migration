70c714fccb535b3e31c998930643589c39513533
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-12-06 15:01:42 +0000

    initial test parsing some json/yaml

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,23 +1,38 @@
-// @global expect 
-// jest.mock('../../src/index')
 // jest.mock('../src/github')
-// import * as google from '../src/google'
-// import * as github from '../src/github'
 import * as mod from '../index'
 
 // let processExitSpy
 // let consoleSpy
 
-// beforeEach(() => {
 
-// })
+describe('parse', () => {
 
-describe('missmatch', () => {
-  // beforeEach(() => {
-  // })
-  it('should have consistent console output', async () => {
-    console.log(mod)
-    return expect(true).toBe(true)
+  it('should parse a yaml object', () => {
+    const yaml = `
+    apiVersion: policy/v1beta1
+    kind: PodSecurityPolicy
+    metadata:
+      name: policy
+    spec:
+      runAsUser:
+        rule: RunAsAny
+      seLinux:
+        rule: RunAsAny
+      fsGroup:
+        rule: MustRunAs
+        ranges:
+          - min: 100
+            max: 200
+      supplementalGroups:
+        rule: 'RunAsAny'
+      volumes:
+        - '*'
+    `
+    return expect(mod.parse(yaml)).toMatchSnapshot()
   })
 
+  it('should parse a json object', () => {
+    const json = `{ "apiVersion": "policy/v1beta1", "kind": "PodSecurityPolicy", "metadata": { "name": "policy" }, "spec": { "runAsUser": { "rule": "RunAsAny" }, "seLinux": { "rule": "RunAsAny" }, "fsGroup": { "rule": "MustRunAs", "ranges": [{ "min": 100, "max": 200 }] }, "supplementalGroups": { "rule": "RunAsAny" }, "volumes": ["*"] } }`
+    return expect(mod.parse(json)).toMatchSnapshot()
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export function getMessage(): string {
-  return 'Hello world!';
+import * as k8s from '@kubernetes/client-node';
+import * as yaml from 'js-yaml';
+
+export function parse(string: string): k8s.V1beta1PodSecurityPolicy {
+  return yaml.load(string) as k8s.V1beta1PodSecurityPolicy
 }
+
