9990e01f6f88a32359754193210cbe4654f28b74
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-12-07 23:22:51 +0000

    working cli

--- src/run.ts
+++ src/run.ts
@@ -1,12 +1,27 @@
 import { readFileSync } from 'fs'
 import * as yaml from 'js-yaml'
 
+import yargs from 'yargs'
+
+const options = yargs
+  .usage("Usage: -e <engine>")
+  .option("engine", {
+    alias: "e",
+    describe: "Policy Engine, must be of [gatekeeper|...]",
+    type: "string",
+    demandOption: true
+  })
+  .argv
+
+
 const psp = readFileSync(0, 'utf-8')
 
 import { parse, transform } from './index'
 
 const parsed = parse(psp)
-const transformed = transform(parsed, 'gatekeeper')
+
+//@ts-ignore
+const transformed = transform(parsed, options.engine)
 
 const yamlString = transformed.map(policy => yaml.dump(policy)).join('\n---\n')
 
