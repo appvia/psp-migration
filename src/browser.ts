8231e784892529c68d23e327a711a3bf580467de
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-12-20 17:34:39 +0000

    pretty ui

--- src/browser.ts
+++ src/browser.ts
@@ -1,20 +1,34 @@
+import 'materialize-css'
+
+import "./styles.scss"
+
 import * as yaml from 'js-yaml'
+
+import hljs from 'highlight.js/lib/core'
+import yamllang from 'highlight.js/lib/languages/yaml'
+
+hljs.registerLanguage('yaml', yamllang)
 
 import { parse, transform } from './index'
 
 const engines = ['gatekeeper', 'kyverno', 'kubewarden']
 
+$(".tabs").tabs()
+
 function process() {
   engines.forEach(engine => {
     try {
       //@ts-expect-error
-      document.getElementById(engine).value = transform(parse(document.getElementById("in").value), engine)
+      let code = transform(parse(document.getElementById("in").value), engine)
         .map(policy => yaml.dump(policy, { noRefs: true, quotingType: '"' }))
         .join('\n---\n')
+      //@ts-expect-error
+      document.getElementById(engine).innerHTML = hljs.highlight(code, { language: 'yaml' }).value
     }
     catch (e) {
+      console.error(e)
       //@ts-expect-error
-      document.getElementById(engine).value = e.message
+      document.getElementById(engine).innerHTML = e.message
     }
   })
 }
@@ -23,8 +37,18 @@ process()
 
 function bugReport() {
   const title = "Bug Report from web-ui"
+
+  const bugurl = new URL("https://github.com/appvia/psp-migration/issues/new")
+
+  bugurl.searchParams.append("template", "bugfromweb.yaml")
+  bugurl.searchParams.append("title", title)
+
   //@ts-expect-error
-  window.open(`https://github.com/appvia/psp-migration/issues/new?template=bugfromweb.yaml&title=${encodeURIComponent(title)}&input=[${encodeURIComponent(document.getElementById("in")?.value)}]&gatekeeper-yaml=[${encodeURIComponent(document.getElementById("gatekeeper").value)}]&kyverno-yaml=[${encodeURIComponent(document.getElementById("kyverno").value)}]&kubewarden-yaml=[${encodeURIComponent(document.getElementById("kubewarden").value)}]`)
+  bugurl.searchParams.append("input", [document.getElementById("in")?.value])
+  //@ts-expect-error
+  engines.forEach(engine => bugurl.searchParams.append(`${engine}-yaml`, [document.getElementById(engine)?.textContent]))
+
+  window.open(bugurl.href)
 }
 
 document.getElementById("in")?.addEventListener("input", process)
